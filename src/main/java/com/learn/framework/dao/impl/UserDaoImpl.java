ad89750c2d41e99924cbd2d690a6f3a901dfa408
shenheya901016 <357924050@qq.com>
2017-05-26 10:09:52 +0800

    提交部分代码

---
+++ src/main/java/com/learn/framework/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package com.learn.framework.dao.impl;
+
+import com.learn.framework.dao.IUserDao;
+
+/**
+ * Created by Administrator on 2017/5/26 0026.
+ */
+public class UserDaoImpl implements IUserDao{
+
+}
