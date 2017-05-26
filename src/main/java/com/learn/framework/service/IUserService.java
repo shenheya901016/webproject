6692795502f2b60477e69d1d76781128d0eb347a
shenheya901016 <357924050@qq.com>
2017-05-26 09:42:58 +0800

    全部commit

---
+++ src/main/java/com/learn/framework/service/IUserService.java
@@ -0,0 +1,9 @@
+package com.learn.framework.service;
+
+/**
+ * Created by Administrator on 2017/4/17 0017.
+ */
+public interface IUserService {
+
+
+}
---
+++ src/main/java/com/learn/framework/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.learn.framework.service.impl;
+
+import com.learn.framework.service.IUserService;
+
+/**
+ * Created by Administrator on 2017/4/17 0017.
+ */
+public class UserServiceImpl implements IUserService {
+
+
+}
